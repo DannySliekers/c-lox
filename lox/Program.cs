d5281b39c08770a677915376fa1202259ce5aae3
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2022-07-06 01:07:34 +0200

    oops

---
+++ lox/Program.cs
@@ -0,0 +1,14 @@
+﻿using lox;
+
+string[] arguments = Environment.GetCommandLineArgs();
+if (arguments.Length > 1)
+{
+    Console.WriteLine("Usage: clox [script]");
+    Environment.Exit(64);
+} else if (arguments.Length == 1)
+{
+    Lox.runFile(arguments[0]);
+} else
+{
+    Lox.runPrompt();
+}
